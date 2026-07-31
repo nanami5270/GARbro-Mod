e9fe8ce7872ab7952e906fdb4ac305fe4c4463c3
scientificworld <scientificworld@users.noreply.github.com>
2026-07-31 23:06:37 +0800

    feat: Mink BINZ archive

--- Legacy/Mink/ArcBINZ.cs
+++ Legacy/Mink/ArcBINZ.cs
@@ -1,8 +1,8 @@
-//! \file       ArcMINK.cs
-//! \date       2018 Feb 01
+//! \file       ArcBINZ.cs
+//! \date       2026-07-31
 //! \brief      Mink resource archive.
 //
-// Copyright (C) 2018 by morkt
+// Copyright (C) 2026 by morkt
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
@@ -27,76 +27,48 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO;
-
-// [000623][Mink] Lovely Angels Peropero Candy 2
+using GameRes.Compression;
 
 namespace GameRes.Formats.Mink
 {
     [Export(typeof(ArchiveFormat))]
-    public class GrpOpener : ArchiveFormat
+    public class DatOpener : ArchiveFormat
     {
-        public override string         Tag { get { return "GRP/MINK"; } }
+        public override string         Tag { get { return "DAT/MINK"; } }
         public override string Description { get { return "Mink resource archive"; } }
-        public override uint     Signature { get { return 0; } }
+        public override uint     Signature { get { return 0x5A4E4942; } } // 'BINZ'
         public override bool  IsHierarchic { get { return false; } }
         public override bool      CanWrite { get { return false; } }
 
-        public GrpOpener ()
-        {
-            Extensions = new string[] { "" };
-        }
-
         public override ArcFile TryOpen (ArcView file)
         {
-            int count = file.View.ReadInt32 (0);
+            int count = file.View.ReadInt32 (4);
             if (!IsSaneCount (count))
                 return null;
-            uint index_offset = file.View.ReadUInt32 (4);
-            if (index_offset < 8 || index_offset >= file.MaxOffset)
-                return null;
 
+            uint index_offset = 8;
             var dir = new List<Entry> (count);
             for (int i = 0; i < count; ++i)
             {
-                var name = file.View.ReadString (index_offset, 0x18);
+                var name = file.View.ReadString (index_offset, 0x20);
                 if (string.IsNullOrWhiteSpace (name))
                     return null;
-                var entry = FormatCatalog.Instance.Create<Entry> (name);
-                entry.Offset = file.View.ReadUInt32 (index_offset+0x18);
-                entry.Size   = file.View.ReadUInt32 (index_offset+0x1C);
+                var entry = FormatCatalog.Instance.Create<PackedEntry> (name);
+                entry.Offset       = file.View.ReadUInt32 (index_offset + 0x20);
+                entry.UnpackedSize = file.View.ReadUInt32 (index_offset + 0x24);
+                entry.Size         = file.View.ReadUInt32 (index_offset + 0x28);
                 if (!entry.CheckPlacement (file.MaxOffset))
                     return null;
                 dir.Add (entry);
-                index_offset += 0x20;
+                index_offset += 0x2C;
             }
             return new ArcFile (file, this, dir);
         }
 
         public override Stream OpenEntry (ArcFile arc, Entry entry)
         {
-            if (!entry.Name.HasExtension (".MSC") || !arc.File.View.AsciiEqual (entry.Offset, "MADSCR"))
-                return base.OpenEntry (arc, entry);
-            var data = arc.File.View.ReadBytes (entry.Offset, entry.Size);
-            ushort id = data.ToUInt16 (8);
-            byte key;
-            if (KnownScriptKeys.TryGetValue (id, out key))
-            {
-                for (int i = 0x20; i < data.Length; ++i)
-                    data[i] ^= key;
-            }
-            return new BinMemoryStream (data, entry.Name);
+            var input = arc.File.CreateStream (entry.Offset, entry.Size);
+            return new LzssStream (input);
         }
-
-        static readonly Dictionary<ushort, byte> KnownScriptKeys = new Dictionary<ushort, byte> {
-            { 0x7E83, 0x66 },
-            { 0x9383, 0x77 },
-            { 0x4E83, 0x88 },
-            { 0xBE82, 0x99 },
-            { 0xA282, 0xAA },
-            { 0xCE82, 0xBB },
-            { 0xAD82, 0xCC },
-            { 0xCD82, 0xDD },
-            { 0xC282, 0xEE },
-        };
     }
 }
